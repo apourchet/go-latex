4abd2dd4ab8e7b35a1d090e76794521f51afb6a0
apourchet <antoine.pourchet@gmail.com>
2013-11-13 15:28:25 -0500

    Changed structure

--- tests/tests.go
+++ tests/tests.go
@@ -7,10 +7,10 @@ import (
 func main() {
 	entries := [][]latex.Entry{{{false, "a"}, {false, "b"}, {false, "c"}, {false, "d"}, {false, "e"}},
 		{{true, "f"}, {true, "g"}, {true, "h"}, {true, "i"}, {true, "j"}}}
-	latex.MakeTable("simple_table.tex", entries)
+	latex.MakeTable("../examples/simple_table.tex", entries)
 	cornerValue := "Mode/Letter"
 	table := latex.Table{cornerValue, []string{"Letter 1", "Letter 1", "Letter 1", "Letter 1", "Letter 1"}, []string{"No Math Mode", "Math Mode"}, entries}
-	latex.MakeTableWithLookup("table_with_lookup.tex", table)
+	latex.MakeTableWithLookup("../examples/table_with_lookup.tex", table)
 
 	linePts := []latex.Point{{"", 0., 0.}, {"", 1., 1.}, {"", 2., 2.}, {"", 3., 3.}}
 	coloredPts := []latex.Point{{"1", 0., 0.}, {"1", 1., 1.}, {"-1", 2., 2.}, {"-1", 3., 3.}}
@@ -19,6 +19,6 @@ func main() {
 	labelToMark["-1"] = "o"
 	labelToMark["1"] = "x"
 
-	latex.MakeLineGraph("line_graph.tex", linePts)
-	latex.MakeColoredPointGraph("point_graph.tex", labelToMark, coloredPts)
+	latex.MakeLineGraph("../examples/line_graph.tex", linePts)
+	latex.MakeColoredPointGraph("../examples/point_graph.tex", labelToMark, coloredPts)
 }
